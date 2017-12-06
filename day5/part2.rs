d302f9d615defbe17230178ff363ff605ca7a26f
Dan Elkouby <streetwalkermc@gmail.com>
2017-12-06 14:40:32 +0200

    Add day 5 solutions

---
+++ day5/part2.rs
@@ -0,0 +1,28 @@
+#!/usr/bin/env run-cargo-script
+
+use std::io::{self,Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    let mut jumps: Vec<isize> = input.lines().map(|l| l.parse().unwrap()).collect();
+    let mut pc = 0;
+    let mut jc = 0;
+
+    loop {
+        if let Some(j) = jumps.get_mut(pc) {
+            pc += *j as usize;
+            if *j >= 3 {
+                *j -= 1;
+            } else {
+                *j += 1;
+            }
+            jc += 1;
+        } else {
+            break
+        }
+    }
+
+    println!("{}", jc);
+}
